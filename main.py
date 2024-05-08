9ee648189f951eec5a35501b9f68eabdbab04996
JonasRocha189 <jonas.rocha189@gmail.com>
2024-05-08 10:35:55 -0300

    crawler loop

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
+import pandas as pd
+
+from sys import setrecursionlimit
+setrecursionlimit(100000)
 
 class Crawler:
 
@@ -10,6 +14,7 @@ class Crawler:
     self.urls = set()
     self.urls_not_crawled = set()
     self.urls_crawled = []
+    self.urls_error = []
 
     self.count = 0
 
@@ -25,10 +30,11 @@ class Crawler:
     if link.netloc == "www.bumper.com" or link.netloc == "":   
 
       # Complete a relative link
-      check = str(link.path)
-      if link.netloc == "" and check[0] == "/":
+      # if link.netloc == "" and str(link.path)[0] == "/":
+      if link.netloc == "":
         url = self.baseURL+link.path
         # print(f"Fixed link: {link}")
+
 
       ## 
       # Check if is a valid URL and standardize the format
@@ -70,69 +76,88 @@ class Crawler:
 
   def crawl(self, url):   
 
-    page = requests.get(url)
-    # print(page.status_code)
+    # print(f"URL to be crawled: {url}")
 
-    #### TODO
-    # Check status, if != 200, create validations
-    soup = BeautifulSoup(page.text, 'lxml')
+    try:
+      page = requests.get(url)
+      # print(page.status_code)
 
-    # Title
-    print("\n")
-    titles = soup.find_all('title')
-    print(f"Titles found: {len(titles)}")
-    for title in titles:
-      print(f"Title: {title.get_text()}")
+      #### TODO
+      # Check status, if != 200, create validations
+      soup = BeautifulSoup(page.text, 'lxml')
 
-    # Description
+      # Title
+      # print("\n")
+      # titles = soup.find_all('title')
+      # print(f"Titles found: {len(titles)}")
+      # for title in titles:
+      #   print(f"Title: {title.get_text()}")
+
+      # Description
 
 
-    # Links
-    print("\n")
-    links = soup.find_all('a')
-    print(f"Links found: {len(links)}")
+      # Links
+      links = soup.find_all('a')
+      # print(f"Links found: {len(links)}")
 
-    for link in links:
-      # remove telephone link
-      if link.get('href').find('tel:'):
-        link.extract()
-      # remove mailto link
-      if link.get('href').find('mailto:'):
-          link.extract()
+      for link in links:
+        # remove telephone link
+        # if link.get('href').find('tel:'):
+        #   link.extract()
+        # # remove mailto link
+        # if link.get('href').find('mailto:'):
+        #     link.extract()
 
-      if 'href' in link.attrs:
-        # print(f"Ancor Text: {link.text} | Link: {link.attrs['href']}" )
-        self.checkUrl(link.attrs['href'])
+        if 'href' in link.attrs:
+          # print(f"Ancor Text: {link.text} | Link: {link.attrs['href']}" )
+          self.checkUrl(link.attrs['href'])
 
-      self.urls_crawled.append(url)
-  
+      
+      print(f"\n\nURL crawled: {url} \nCounter: {self.count}")
+      self.urls_crawled.append(url)  
+    except:
+      self.urls_error.add(url)
+      print(f"Error crawling page: {url}")
     
-    print(f"URL crawlled: {url} \nCounter: {self.count}")
     self.count += 1    
     self.start()
+
+    
 
 
   def start(self):
 
     if len(self.urls) == 0: 
       self.urls.add(self.baseURL)
-      self.crawl(self.baseURL) ### check the homepage
+      # self.crawl(self.baseURL) ### check the homepage
       
-    # if self.count >= 10:
-    #   print("End of the crawler")
-    # else:
-    #   newUrl = list(self.urls)
-    #   self.crawl(newUrl[self.count])
-
+    if self.count >= 1000:
+      print("///////////////////////////")
+      print("End of the crawler")
 
       ### Check Crawled URL's
-      print(f"\n\nURLS ({len(self.urls)}):")
-      for l in self.urls:
+      print(f"\n\nURLS Crawled({len(self.urls_crawled)}):")
+      for l in self.urls_crawled:
         print(l)
 
       print(f"\n\nURLS NOT CRAWLED ({len(self.urls_not_crawled)}):")
       for n in self.urls_not_crawled:
-        print(n)
+        print(n)  
+
+      print(f"\n\nLINKS NOT FOUND ({len(self.urls)}):")
+      for n in self.urls:
+        print(n) 
+
+      print(f"\n\nURL's WITH ERROR ({len(self.urls_error)}):")
+      for n in self.urls:
+        print(n)  
+
+    else:
+      newUrl = list(self.urls)
+      self.crawl(newUrl[self.count])
+
+
+    
 
 
 if __name__ == "__main__":
